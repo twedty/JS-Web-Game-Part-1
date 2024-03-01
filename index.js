728f64a68f87351408a794f45cbc0f47f8e768e5
twedty <jamestwedt@gmail.com>
2024-03-01 18:22:39 -0500

    sharpening the refactoring to include less lines of code (noticed a lot of repetition

--- index.js
+++ index.js
@@ -19,39 +19,39 @@ function newImage(src, left, bottom) {
     let character = document.createElement('img'); //used character as a generic way of adding each new image/character to the function.
     character.src = src; //made the src = src so that each new character has a parameter that changes
     character.style.position = 'fixed';
-    character.style.left = left; //changes
-    character.style.bottom = bottom; //changes
+    character.style.left = left + 'px'; //changes and added px for less repetition later
+    character.style.bottom = bottom + 'px'; //changes and ^^
     document.body.append(character);
 }
 
 let greenCharacterSrc = 'assets/green-character.gif';  //you can see the src, left, and bottom as changing parameters with each new character being added
-let greenCharacterLeft = '100px';
-let greenCharacterBottom = '100px';
+let greenCharacterLeft = '100';
+let greenCharacterBottom = '100';
 newImage(greenCharacterSrc, greenCharacterLeft, greenCharacterBottom);
 
 let pineTreeSrc = 'assets/pine-tree.png';
-let pineTreeLeft = '450px';
-let pineTreeBottom = '200px';
+let pineTreeLeft = '450';
+let pineTreeBottom = '200';
 newImage(pineTreeSrc, pineTreeLeft, pineTreeBottom);
 
 let treeSrc = 'assets/tree.png';
-let treeLeft = '200px';
-let treeBottom = '300px';
+let treeLeft = '200';
+let treeBottom = '300';
 newImage(treeSrc, treeLeft, treeBottom);
 
 let pillarSrc = 'assets/pillar.png';
-let pillarLeft = '350px';
-let pillarBottom = '100px';
+let pillarLeft = '350';
+let pillarBottom = '100';
 newImage(pillarSrc, pillarLeft, pillarBottom);
 
 let crateSrc = 'assets/crate.png';
-let crateLeft = '150px';
-let crateBottom = '200px';
+let crateLeft = '150';
+let crateBottom = '200';
 newImage(crateSrc, crateLeft, crateBottom);
 
 let wellSrc = 'assets/well.png';
-let wellLeft = '500px';
-let wellBottom = '425px';
+let wellLeft = '500';
+let wellBottom = '425';
 newImage(wellSrc, wellLeft, wellBottom);
 //done with newImage
 //starting newItem
@@ -60,24 +60,12 @@ newImage(wellSrc, wellLeft, wellBottom);
 
 
 function newItem(src, left, bottom){
-    newImage(src, left, bottom) //included src, left, and bottom on both functions to be able to declare in the newItem function that I was also calling for the src, left, and bottom parameters in the newImage function.
+    let character = newImage(src, left, bottom); //included src, left, and bottom on both functions to be able to declare in the newItem function that I was also calling for the src, left, and bottom parameters in the newImage function.
+    character.addEventListener('dblclick', () => {
+        character.remove()
+    })
 }
 
-let swordSrc = 'assets/sword.png';
-let swordLeft = '500px';
-let swordBottom = '405px';
-newItem(swordSrc, swordLeft, swordBottom)
-
-let shieldSrc = 'assets/shield.png';
-let shieldLeft = '165px';
-let shieldBottom = '185px';
-newItem(shieldSrc, shieldLeft, shieldBottom)
-
-let staffSrc = 'assets/staff.png';
-let staffLeft = '600px';
-let staffBottom = '100px';
-newItem(staffSrc, staffLeft, staffBottom)
-
-sword.addEventListener('dblclick', function(){
-    sword.remove()
-})
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
